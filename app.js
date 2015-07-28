780b4e82fde80f9e9986230c5a32401e06b32d96
Bryan Duplantis <duplantis@gmail.com>
2015-07-28 14:19:29 -0500

    updated code based on John Quarles code on GitHub

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// var args = process.argv.slice(2);
-// var cal = require('./lib/makecal');
+var args = process.argv.slice(2);
+var cal = require('./lib/makecal');
 
-// cal.makeCal(args);
+cal.makeCal(args);
